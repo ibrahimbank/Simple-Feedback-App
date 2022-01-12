ca2443ec6d3e5aa79103448377f40a4eee7fb0a2
bankole <bankoleibrahim96@gmail.com>
2022-01-12 14:22:32 +0100

    Added the update, delete, and add functionalities to the ui

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -19,9 +19,7 @@ export const FeedBackProvider = ({ children }) => {
 
   //Fetch feedback
   const fetchFeedback = async () => {
-    const response = await fetch(
-      `  http://localhost:5000/feedback/?_sort=id&_order=desc`
-    );
+    const response = await fetch(`/feedback/?_sort=id&_order=desc`);
     const data = await response.json();
     setFeedback(data);
     setIsLoading(false);
@@ -36,22 +34,44 @@ export const FeedBackProvider = ({ children }) => {
   };
 
   // Delete Feedback
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
+      await fetch(`/feedback/${id}`, {
+        method: "DELETE",
+      });
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
 
   // Add Feedback
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch("/feedback", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newFeedback),
+    });
+
+    const data = await response.json();
+    // newFeedback.id = uuidv4();
+    setFeedback([data, ...feedback]);
   };
 
   // update feedbackitems
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updItem),
+    });
+
+    const data = await response.json();
+
     setFeedback(
-      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
     );
   };
 
