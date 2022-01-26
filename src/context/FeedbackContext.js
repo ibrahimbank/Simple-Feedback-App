00bfc0bb93131a2fc68b47218605986be40a6303
bankole <bankoleibrahim96@gmail.com>
2022-01-26 18:00:06 +0100

    Changed setup

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,5 +1,5 @@
 import { createContext, useState, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
+// import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 // const [feedback, setFeedback] = useState(FeedbackData);
